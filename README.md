662900908be98c2af1a563b1cc00dbf27e380f4b
Jia Huang <iamfat@gmail.com>
2017-10-19 09:59:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pug Template Compiler for Gini
+
+## How to use it?
+把`pug`作为依赖模块加入`gini.json`, 然后在应用的`view`目录直接通过扩展名为`.pug`的文件来使用这一渲染器.
---
+++ class/Gini/View/Pug.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Gini\View;
+
+class Pug implements Engine
+{
+    private $_path;
+    private $_vars;
+
+    private static $_pug;
+
+    public function __construct($path, array $vars)
+    {
+        $this->_path = $path;
+        $this->_vars = $vars;
+        if (!self::$_pug) {
+            self::$_pug = new Pug(['pretty' => true, 'cache'=> self::cacheDir()]);
+        }
+    }
+
+    public function __toString()
+    {
+        if ($this->_path) {
+            $output = self::$_pug->render($this->_path, $this->_vars);
+        }
+
+        return $output;
+    }
+
+    public static function cacheDir() {
+        return \Gini\Config::get('pug.cache_dir') ?: sys_get_temp_dir().'/gini-pug';
+    }
+}
